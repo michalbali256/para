4c7ff7470222a5eece9d2345ad0071cd8b7a73b2
Michal Bali <michal.bali@broadcom.com>
2019-03-19 16:47:37 +0100

    asd

--- 01-kmeans/framework/implementation.hpp
+++ 01-kmeans/framework/implementation.hpp
@@ -13,12 +13,14 @@
 template<typename POINT = point_t, typename ASGN = std::uint8_t, bool DEBUG = false>
 class KMeans : public IKMeans<POINT, ASGN, DEBUG>
 {
-public:
-
 	static const std::vector<POINT> * points;
 	static size_t k;
-	static std::vector<ASGN> * assignments;
+	static std::vector<ASGN> assignments;
 	static std::vector<POINT> * centroids;
+
+public:
+
+	
 	/*
 	 * \brief Perform the initialization of the functor (e.g., allocate memory buffers).
 	 * \param points Number of points being clustered.
@@ -27,12 +29,12 @@ public:
 	 */
 	virtual void init(std::size_t points, std::size_t k, std::size_t iters)
 	{
+		assignments.resize(points);
 		/*
 			Your core goes here ...
 		 */
 	}
 
-	template <bool update_assignments>
 	struct sum {
 		std::vector<POINT> point_sum;
 		std::vector<int_fast64_t> centroid_count;
@@ -49,29 +51,42 @@ public:
 			centroid_count.resize(k);
 		}
 
-		void operator()(const tbb::blocked_range<POINT*>& r) {
-			
-			for (POINT* a = r.begin(); a != r.end(); ++a)
+		typedef typename POINT::coord_t coord_t;
+
+		static coord_t distance(const POINT &point, const POINT &centroid)
+		{
+			std::int64_t dx = (std::int64_t)point.x - (std::int64_t)centroid.x;
+			std::int64_t dy = (std::int64_t)point.y - (std::int64_t)centroid.y;
+			return (coord_t)(dx*dx + dy * dy);
+		}
+
+		static std::size_t getNearestCluster(const POINT &point, const std::vector<POINT> &centroids)
+		{
+			coord_t minDist = distance(point, centroids[0]);
+			std::size_t nearest = 0;
+			for (std::size_t i = 1; i < centroids.size(); ++i)
 			{
-				float m;
-				size_t mi;
-				for (size_t i = 0; i < k; ++i)
-				{
-					float dist = sqrt((a->x - (*centroids)[i].x) * (a->x - (*centroids)[i].x) + (a->y - (*centroids)[i].y) * (a->y - (*centroids)[i].y));
-					if (dist < m)
-					{
-						mi = i;
-						m = dist;
-					}
+				coord_t dist = distance(point, centroids[i]);
+				if (dist < minDist) {
+					minDist = dist;
+					nearest = i;
 				}
+			}
+
+			return nearest;
+		}
+
+		void operator()(const tbb::blocked_range<const POINT*>& r)
+		{
+			
+			for (const POINT* a = r.begin(); a != r.end(); ++a)
+			{
+				size_t mi = getNearestCluster(*a, *centroids);
+
 				point_sum[mi].x += a->x;
 				point_sum[mi].y += a->y;
 				centroid_count[mi]++;
-				if (update_assignments)
-				{
-
-				}
-
+				assignments[a - points->data()] = (ASGN) mi;
 			}
 			
 		}
@@ -101,20 +116,46 @@ public:
 	virtual void compute(const std::vector<POINT> &points, std::size_t k, std::size_t iters,
 		std::vector<POINT> &centroids, std::vector<ASGN> &assignments)
 	{
+		
+		
+		centroids.resize(k);
+		for (std::size_t i = 0; i < k; ++i)
+			centroids[i] = points[i];
+
+		KMeans::points = &points;
+		KMeans::centroids = &centroids;
+		KMeans::k = k;
+
 		for (size_t i = 0; i < iters; ++i)
 		{
-			sum<false> s;
+			sum s;
+			tbb::blocked_range<const POINT *> whole(points.data(), points.data() + points.size());
+
+			tbb::parallel_reduce(whole, s);
+
+			for (std::size_t i = 0; i < k; ++i)
+			{
+				if (s.centroid_count[i] == 0) continue;	// If the cluster is empty, keep its previous centroid.
+				centroids[i].x = s.point_sum[i].x / (std::int64_t)s.centroid_count[i];
+				centroids[i].y = s.point_sum[i].y / (std::int64_t)s.centroid_count[i];
+			}
 		}
-		
-		
-		/*
-			Your core goes here ...
-		*/
-		throw bpp::RuntimeError("Solution not implemented yet.");
+
+		assignments = std::move(KMeans::assignments);
+
 	}
 };
 
 template<typename POINT, typename ASGN, bool DEBUG>
 size_t KMeans<POINT, ASGN, DEBUG>::k;
+
+template<typename POINT, typename ASGN, bool DEBUG>
+const std::vector<POINT> * KMeans<POINT, ASGN, DEBUG>::points;
+
+template<typename POINT, typename ASGN, bool DEBUG>
+std::vector<ASGN> KMeans<POINT, ASGN, DEBUG>::assignments;
+
+template<typename POINT, typename ASGN, bool DEBUG>
+std::vector<POINT> * KMeans<POINT, ASGN, DEBUG>::centroids;
 
 #endif
